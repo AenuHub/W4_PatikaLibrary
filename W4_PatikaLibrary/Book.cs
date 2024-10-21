f7570ccde5f14c26713d2de093f9e323d2885b18
AenuHub <jesuskane92@gmail.com>
2024-10-21 14:56:16 +0300

    Add project files.

---
+++ W4_PatikaLibrary/Book.cs
@@ -0,0 +1,47 @@
+﻿namespace W4_PatikaLibrary
+{
+    public class Book
+    {
+        private string title;
+        private string authorFirstName;
+        private string authorLastName;
+        private int pageCount;
+        private string publisher;
+        private DateTime savedDate;
+
+        // getters and setters
+        public string Title { get { return title; } set { title = value; } }
+        public string AuthorFirstName { get { return authorFirstName; } set { authorFirstName = value; } }
+        public string AuthorLastName { get { return authorLastName; } set { authorLastName = value; } }
+        public int PageCount { get { return pageCount; } set { pageCount = value; } }
+        public string Publisher { get { return publisher; } set { publisher = value; } }
+        public DateTime SavedDate { get { return savedDate; } set { savedDate = value; } }
+
+        // default empty constructor
+        public Book()
+        {
+            SavedDate = DateTime.Now;
+        }
+
+        // constructor with parameters
+        public Book(string title, string authorFirstName, string authorLastName, int pageCount, string publisher)
+        {
+            Title = title;
+            AuthorFirstName = authorFirstName;
+            AuthorLastName = authorLastName;
+            PageCount = pageCount;
+            Publisher = publisher;
+            SavedDate = DateTime.Now;
+        }
+
+        // method to print book details
+        public void PrintBookDetails()
+        {
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Author: {AuthorFirstName} {AuthorLastName}");
+            Console.WriteLine($"Page Count: {PageCount}");
+            Console.WriteLine($"Publisher: {Publisher}");
+            Console.WriteLine($"Saved Date: {SavedDate.ToShortDateString()}");
+        }
+    }
+}
---
+++ W4_PatikaLibrary/Program.cs
@@ -0,0 +1,21 @@
+﻿using W4_PatikaLibrary;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Book book = new Book("Adı Aylin", "Ayşe", "Kulin", 398, "Remzi Kitapevi");
+        book.PrintBookDetails();
+
+        /* classes are blueprints for creating objects, allowing us to define properties and methods,
+        also behaviours that objects of that class will have. */
+
+        /* properties are special class members that provide read, write or compute the values of 
+         private fields, allowing for encapsulation and data validation. */
+
+        /* new keyword is used to create instances of a class and invoking its constructor. */
+
+        /* a constuctor is a special method that is called automatically when an instance of a class
+         is created, allowing for initialization of the object's properties. */
+    }
+}
